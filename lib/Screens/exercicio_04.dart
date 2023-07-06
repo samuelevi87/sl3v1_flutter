9ce286026c793437cbc1d075f488fa7f07fb5279
samuelevi87 <samuel.levi.alves@outlook.com>
2023-07-06 08:03:19 -0300

    doc: Atualização do README.md

--- lib/Screens/exercicio_04.dart
+++ lib/Screens/exercicio_04.dart
@@ -191,7 +191,6 @@ class _Exercicio04State extends State<Exercicio04> {
   }
 }
 
-// creating Stateless Widget for buttons
 class MyButton extends StatelessWidget {
   final color;
   final textColor;
